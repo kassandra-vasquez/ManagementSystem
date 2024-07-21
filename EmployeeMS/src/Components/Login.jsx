e404c4f8202bf717526b71f5a77f951dbbb6382d
kassandra-vasquez <kyanelyyy@gmail.com>
2024-07-21 13:53:23 -0700

    admin info in server

--- EmployeeMS/src/Components/Login.jsx
+++ EmployeeMS/src/Components/Login.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import axios from "axios";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/auth/adminlogin", values)
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
       <div className="p-3 rounded w-25 border loginForm">
         <h2>Login Page</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email:</strong>
@@ -17,6 +31,7 @@ const Login = () => {
               autoComplete="off"
               placeholder="Enter Email"
               className="form-control rounded-0"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
             />
           </div>
           <div className="mb-3">
@@ -29,11 +44,16 @@ const Login = () => {
               autoComplete="off"
               placeholder="Enter Password"
               className="form-control rounded-0"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
             />
           </div>
-          <button className="btn btn-success w-100 rounded-0 mb-2">Log in</button>
+          <button className="btn btn-success w-100 rounded-0 mb-2">
+            Log in
+          </button>
           <div className="mb-1">
-            <input type="checkbox" name="tick" id="tick" className="me-2"/>
+            <input type="checkbox" name="tick" id="tick" className="me-2" />
             <label htmlFor="password">Agree to Terms and Conditions</label>
           </div>
         </form>
