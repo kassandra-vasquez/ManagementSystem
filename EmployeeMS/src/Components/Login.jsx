f9a70423c922f582026758302e40ad8aa4ffa186
kassandra-vasquez <kyanelyyy@gmail.com>
2024-08-02 08:33:59 -0700

    added menu to dashboard page

--- EmployeeMS/src/Components/Login.jsx
+++ EmployeeMS/src/Components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
       <div className="p-3 rounded w-25 border loginForm">
-        <div className="text-danger">{error && error}</div>
+        <div className="text-warning">{error && error}</div>
         <h2>Login Page</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
