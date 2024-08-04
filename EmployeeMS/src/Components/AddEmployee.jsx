9a626c3a172f628a1bf272e9dbfc229a3cb67eab
kassandra-vasquez <kyanelyyy@gmail.com>
2024-08-04 13:37:17 -0700

    add employee going through and saving in sql

--- EmployeeMS/src/Components/AddEmployee.jsx
+++ EmployeeMS/src/Components/AddEmployee.jsx
@@ -2,6 +2,16 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const AddEmployee = () => {
+  const [employee, setEmployee] = useState({
+    name: "",
+    email: "",
+    password: "",
+    salary: "",
+    address: "",
+    category_id: "",
+    image: "",
+  });
+
   const [category, setCategory] = useState([]);
 
   useEffect(() => {
@@ -17,13 +27,30 @@ const AddEmployee = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", employee.name)
+    formData.append("email", employee.email)
+    formData.append("password", employee.password)
+    formData.append("address", employee.address)
+    formData.append("salary", employee.salary)
+    formData.append("image", employee.image)
+    formData.append("category_id", employee.category_id)
+
+    axios
+      .post("http://localhost:3000/auth/add_employee", formData)
+      .then((result) => console.log(result.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center mt-3">
       <div className="p-3 rounded w-50 border">
         <h3 className="text-center">Add Employee</h3>
-        <form className="row g-1">
+        <form className="row g-1" onSubmit={handleSubmit}>
           <div className="col-12">
-            <label for="inputName" className="form-label">
+            <label htmlFor="inputName" className="form-label">
               Name
             </label>
             <input
@@ -31,10 +58,13 @@ const AddEmployee = () => {
               id="inputName"
               placeholder="Enter Name"
               className="form-control rounded-0"
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
             />
           </div>
           <div className="col-12">
-            <label for="inputEmail4" className="form-label">
+            <label htmlFor="inputEmail4" className="form-label">
               Email
             </label>
             <input
@@ -43,10 +73,13 @@ const AddEmployee = () => {
               id="inputEmail4"
               autoComplete="off"
               placeholder="Enter Email"
+              onChange={(e) =>
+                setEmployee({ ...employee, email: e.target.value })
+              }
             />
           </div>
           <div className="col-12">
-            <label for="inputPassword4" className="form-label">
+            <label htmlFor="inputPassword4" className="form-label">
               Password
             </label>
             <input
@@ -54,8 +87,11 @@ const AddEmployee = () => {
               className="form-control rounded-0"
               id="inputPassword4"
               placeholder="Enter Password"
+              onChange={(e) =>
+                setEmployee({ ...employee, password: e.target.value })
+              }
             />
-            <label for="inputSalary" className="form-label">
+            <label htmlFor="inputSalary" className="form-label">
               Salary
             </label>
             <input
@@ -64,20 +100,13 @@ const AddEmployee = () => {
               id="inputSalary"
               placeholder="Enter Salary"
               autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, salary: e.target.value })
+              }
             />
           </div>
           <div className="col-12">
-            <label for="category" className="form-label">
-              Category
-            </label>
-            <select name="category" id="category" className="form-select">
-              {category.map((c) => {
-                return <option value={c.name}>{c.name}</option>;
-              })}
-            </select>
-          </div>
-          <div className="col-12">
-            <label for="inputAddress" className="form-label">
+            <label htmlFor="inputAddress" className="form-label">
               Address
             </label>
             <input
@@ -85,21 +114,51 @@ const AddEmployee = () => {
               className="form-control rounded-0"
               id="inputAddress"
               autoComplete="off"
-              placeholderS="1234 Main St"
+              placeholder="1234 Main St"
+              onChange={(e) =>
+                setEmployee({ ...employee, address: e.target.value })
+              }
             />
           </div>
+          <div className="col-12">
+            <label htmlFor="category" className="form-label">
+              Category
+            </label>
+            <select
+              id="category"
+              name="category"
+              className="form-select"
+              onChange={(e) =>
+                setEmployee({ ...employee, category_id: e.target.value })
+              }
+            >
+              {category.map((c) => {
+                return (
+                  <option key={c.id} value={c.id}>
+                    {c.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
           <div className="col-12 mb-3">
-            <label className="form-label" for="inputGroupFile01">
+            <label className="form-label" htmlFor="inputGroupFile01">
               Select Image
             </label>
             <input
               type="file"
+              name="image"
               className="form-control rounded-0"
               id="inputGroupFile01"
+              onChange={(e) =>
+                setEmployee({ ...employee, image: e.target.files[0] })
+              }
             />
           </div>
           <div className="col-12">
-            <button className="btn btn-primary w-100">Add Employee</button>
+            <button type="submit" className="btn btn-primary w-100">
+              Add Employee
+            </button>
           </div>
         </form>
       </div>
--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -1,6 +1,9 @@
 import express from "express";
 import con from "../utils/db.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
@@ -26,16 +29,53 @@ router.post("/adminlogin", (req, res) => {
 router.get("/category", (req, res) => {
   const sql = "SELECT * FROM category";
   con.query(sql, (err, result) => {
-    if (err) return res.json({Status: false, Error: "Query Error"})
-      return res.json({Status: true, Result: result})
-  })
-})
+    if (err) return res.json({ Status: false, Error: "Query Error" });
+    return res.json({ Status: true, Result: result });
+  });
+});
 
 router.post("/add_category", (req, res) => {
   const sql = "INSERT INTO category (`name`) VALUES (?)";
   con.query(sql, [req.body.category], (err, result) => {
     if (err) return res.json({ Status: false, Error: "Query Error" });
     return res.json({ Status: true });
+  });
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Public/Images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.filename + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({
+  storage: storage
+});
+
+router.post("/add_employee", upload.single("image"), (req, res) => {
+  const sql = `INSERT INTO employee
+    (name, email, password, address, salary, image, category_id)
+    VALUES (?)`;
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) return res.json({ Status: false, Error: "Query Error" });
+    const values = [
+      req.body.name,
+      req.body.email,
+      hash,
+      req.body.address,
+      req.body.salary,
+      req.file.filename,
+      req.body.category_id,
+    ];
+    con.query(sql, [values], (err, result) => {
+      if (err) return res.json({ Status: false, Error: err });
+      return res.json({ Status: true });
+    });
   });
 });
 
